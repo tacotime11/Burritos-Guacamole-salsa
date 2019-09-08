fd6b90bc5e3a72079d56b826ceaff6a82354cea4
tacotime11 <coryrichardschmidt@gmail.com>
2019-09-08 13:48:37 +0200

    put in a new structure for the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,11 @@
 var $pokemonRepository = (function() {
-    function getAll() {
-  var repository = [];
-  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-})
-
   function getAll() {
-    return repository;
   }
 
   function showDetails(pokemon) {
-      $pokemonRepository.loadDetails(pokemon)
   }
 
   function loadList(){
-    return .ajax(apiUrl, {dataType: 'json'}).then(function(item){
-      .each(item.results, function(index, item){
-        var pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        }
-        add(pokemon)
-      })
-      }).catch(function(e){
-      console.error(e);
-    });
   }
 
   function add(pokemon) {
@@ -31,65 +13,20 @@ var $pokemonRepository = (function() {
   }
 
   function addListItem(pokemon){
-    var listItem = ();
-    listItem.text(pokemon.name);
-    pokemonList.append(listItem);
-    listItem.click(function() {
-      showDetails(pokemon)
-    });
   }
 
   function loadDetails(item) {
-    var url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
 
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = Object.keys(details.types);
-      return item;
-    }).catch(function (e) {
-      console.error(e);
-    });
   }
 
-  var modalContainer = document.querySelector('#modal-container');
-  var dialogPromiseReject;
-
   function showModal(item) {
-
-    var modal = document.createElement('div');
-    modal.addclass('modal');
+  }
 
 
-    function showDetails(pokemon){
-      pokemonRepository.loadDetails(pokemon).then(function() {
-        var modal = $('.modal-body');
-        var name = $('.modal-title').text(pokemon.name);
-        var height = $('modal.height>').text(pokemon.height);
-        var image = $('<img class="pokemon-image">');
-        image.attr('src', pokemon.imageUrl);
-
-
-        if(modal.children().length) {
-          modal.children().remove();
-        }
-
-        modal.append(image)
-             .append(height)
-             .append(type);
-
-      });
-    }
+  function showDetails(pokemon){
+  }
 
   function hideModal() {
-    $modalContainer.classList.remove('is-visible');
-
-    if (dialogPromiseReject) {
-      dialogPromiseReject();
-      dialogPromiseReject = null;
-    }
   }
 
   return {
